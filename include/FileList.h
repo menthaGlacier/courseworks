b7018f6925789f8d8f2c742f17cd4780f932e4ad
Andrew Litvinenko <dandeliondevelop@gmail.com>
2023-01-02 04:33:55 +0700

    Added list pointers overwriting. Broken build

--- include/FileList.h
+++ include/FileList.h
@@ -6,6 +6,8 @@
 
 template <class T> class FileList {
 public:
+	// Конструктор файлового списка по умолчанию. Вначале пробует открыть файл
+	// либо создать его, если открытие не осуществилось.
 	FileList() {
 		file.open("CWBin", std::ios::binary | std::ios::in | std::ios::out);
 		if (!file.is_open()) {
@@ -43,34 +45,35 @@ public:
 		}
 	}
 
+	// Вставка элемента в конец списка
 	void insert(const T& _data) {
 		Node<T> tail;
 		int64_t pos;
 
+		file.clear();
+		file.seekg(0, std::ios::end);
+		pos = file.tellg();
+
+		// Если список пуст, первый элемент будет после указателей списка
 		if (size == 0) {
-			file.seekg(0);
-			first = sizeof(first) * 2; last = sizeof(last) * 2;
-			tail.data = _data; tail.prev = -1; tail.next = -1;
-			file.write(reinterpret_cast<char*>(&first), sizeof(first));
-			file.write(reinterpret_cast<char*>(&last), sizeof(last));
-			file.write(reinterpret_cast<char*>(&tail.prev), sizeof(tail.prev));
-			file.write(reinterpret_cast<char*>(&tail.next), sizeof(tail.next));
-			file.write(reinterpret_cast<char*>(&tail.data), sizeof(tail.data));
-			size += 1;
+			first = sizeof(first) + sizeof(last);
+		} else {
+			file.seekg(last);
+			// TODO
 		}
 
-		//
-		file.seekg(last);
-		file.seekg(0, std::ios::end);
 		tail.data = _data; tail.prev = last; tail.next = -1;
-		file.write(reinterpret_cast<char*>(&tail.prev), sizeof(tail.prev));
-		file.write(reinterpret_cast<char*>(&tail.next), sizeof(tail.next));
-		file.write(reinterpret_cast<char*>(&tail.data), sizeof(tail.data));
+		last = file.tellg();
+		overwriteListPointers();
+		file.seekg(0, std::ios::end);
+		tail.write(file);
 		size += 1;
 	}
 
 private:
-	//
+	// Используется для (пере)создания файла, если его не существует или доступ
+	// к нему не может быть осуществлен. В самом начале записывает указатели
+	// списка со значениями -1. Возвращает файловый указатель на начало файла
 	void createAndOpenFile() {
 		file.open("CWBin", std::ios::out);
 		if (!file.is_open()) {
@@ -86,11 +89,18 @@ private:
 		}
 
 		first = -1, last = -1;
-		file.write(reinterpret_cast<char*>(&first), sizeof(first));
-		file.write(reinterpret_cast<char*>(&last), sizeof(last));
+		overwriteListPointers();
 		file.seekg(0);
 	}
 
+	// Перезаписывает указатели списка first и last на актуальные версии,
+	// оставляет файловый указатель на первом записанном элементе в файле
+	void overwriteListPointers() {
+		file.clear();
+		file.seekg(0);
+		file.write(reinterpret_cast<char*>(&first), sizeof(first));
+		file.write(reinterpret_cast<char*>(&last), sizeof(last));
+	}
 private:
 	std::fstream file;
 	int64_t first, last;
