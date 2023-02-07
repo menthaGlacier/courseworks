386dd0299b8c064bc0136ecbb5f6b490241c54c6
Andrew Litvinenko <dandeliondevelop@gmail.com>
2023-02-07 18:09:19 +0700

    Added menu

---
+++ source/Interface.cpp
@@ -0,0 +1,49 @@
+#include "../include/Interface.h"
+
+Interface::Interface() {
+	listType = ListType::None;
+	intList = nullptr;
+	doubleList = nullptr;
+	stringList = nullptr;
+
+	displayListSelectionMenu();
+}
+
+void Interface::displayMenu() {
+	std::cout << "Menu" << std::endl;
+
+	if (listType != ListType::None) {
+	std::cout << "Current list name: ";
+		switch (listType) {
+		case ListType::isInt:
+			std::cout << intList->name << std::endl;
+			break;
+		case ListType::isDouble:
+			std::cout << doubleList->name << std::endl;
+			break;
+		case ListType::stringType:
+			std::cout << stringList->name << std::endl;
+			break;
+		}
+	} else {
+		std::cout << "List not selected" << std::endl;
+		displayListSelectionMenu();
+	}
+
+	std::cout << "Avaliable actions:" << std::endl << "\t"
+		<< "[0] - Change working list" << "\t"
+		<< "[1] - Insert element" << "\t"
+		<< "[2] - Remove element" << "\t"
+		<< "[3] - Change element" << "\t"
+		<< "[4] - Find element" << "\t"
+		<< "[5] - Output list" << "\t"
+		<< "[6] - Sort list" << "\t"
+		<< "[7] - Quit" << std::endl;
+}
+
+void Interface::displayListSelectionMenu() {
+	std::cout << "Avaliable actions:" << std::endl << "\t"
+		<< "[1] - Create a new list" << "\t"
+		<< "[2] - Open an existing list" << "\t"
+		<< "[3] - Quit" << std::endl;
+}
