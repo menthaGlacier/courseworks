7cd40063f856a1c746b58822a178d39a4a8e3bee
Andrew Litvinenko <dandeliondevelop@gmail.com>
2023-08-26 18:18:34 +0700

    Changed private fields and methods on Node to public

--- include/Node.h
+++ include/Node.h
@@ -4,7 +4,7 @@
 #include "dependencies.h"
 
 template <class TT> class Node {
-private:
+public:
 	bool write(std::fstream& file) {
 		if (!file.is_open()) {
 			return false;
@@ -12,9 +12,9 @@ private:
 		
 		file.clear();
 
-		file.write(reinterpret_cast<char*>(&prev), sizeof(prev));
-		file.write(reinterpret_cast<char*>(&next), sizeof(next));
-		file.write(reinterpret_cast<char*>(&data), sizeof(data));
+		file.write(reinterpret_cast<const char*>(&prev), sizeof(prev));
+		file.write(reinterpret_cast<const char*>(&next), sizeof(next));
+		file.write(reinterpret_cast<const char*>(&data), sizeof(data));
 	
 		if (file.fail()) {
 			return false;
@@ -49,18 +49,9 @@ private:
 		return totalSize;
 	}
 
-	bool operator<(const Node<TT>& l, const Node<TT>& r) const {
-		return l.data < r.data;
-	}
-
-	bool operator>(const Node<TT>& l, const Node<TT>& r) const {
-		return l.data > r.data;
-	}
-
-private:
+public:
 	TT data;
 	int64_t prev, next;
-	template <class T> friend class FileList;
 };
 
 #endif /* NODE_H */
